e986f07f808e01d0f4516a3881b796cac5e99e3e
Brian Jenkins <brianjenkins94@gmail.com>
2025-07-22 13:34:50 -0400

    Argument order?

--- scripts/build.ts
+++ scripts/build.ts
@@ -21,7 +21,7 @@ export async function build(workspaces?) {
 
     return Object.fromEntries(await mapAsync(workspaces, function(workspace) {
         return new Promise(function(resolve, reject) {
-            const subprocess = spawn("npm", ["run", "build", "--if-present"], {
+            const subprocess = spawn("npm", ["run", "--if-present", "build"], {
                 "cwd": workspace,
                 "shell": true,
                 //"stdio": "inherit"
