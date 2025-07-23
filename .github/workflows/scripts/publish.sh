bc2dd0c243edd8e50dec1e0b84a36e8daa03d38a
Brian Jenkins <brianjenkins94@gmail.com>
2025-07-22 21:29:59 -0400

    Keep promotion logic in actions

--- .github/workflows/scripts/publish.sh
+++ .github/workflows/scripts/publish.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
-PACKAGE="$1"
-
-TAG_NAME=$(jq --raw-output 'select(.version != null and .version != "") | "\(.name)@\(.version)"' "$PACKAGE/package.json" || true)
-
+PACKAGES=("$@")
 RELEASES=$(gh release list --json tagName)
 
-if [[ -z "$TAG_NAME" ]] || jq --raw-output '.[].tagName' <<< "$RELEASES" | grep -qx "$TAG_NAME"; then
-  TAG_NAME="${PACKAGE}@$(jq --raw-output '([.[] | select(.tagName | startswith("'"$PACKAGE"'@")) | .tagName | sub("'"$PACKAGE"'@";"") | select(length > 0) | split(".") | map(tonumber)] | sort | last | if . == null or . == [] then [0,1,0] else . end | map(tostring) | join("."))' <<< "$RELEASES")"
-fi
+pnpm run publish
 
-echo "📦 Publishing $PACKAGE as $TAG_NAME"
+for PACKAGE in "${PACKAGES[@]}"; do
+  TAG_NAME=$(jq --raw-output 'select(.version != null and .version != "") | "\(.name)@\(.version)"' "$PACKAGE/package.json" || true)
 
-if pnpm run publish; then
-  if [[ ! -f "docs/${TAG_NAME}.tgz" ]]; then
-    echo "❌ Archive $TAG_NAME not found"
-    exit 1
+  if [[ -z "$TAG_NAME" ]] || jq --raw-output '.[].tagName' <<< "$RELEASES" | grep -qx "$TAG_NAME"; then
+    TAG_NAME="${PACKAGE}@$(jq --raw-output '([.[] | select(.tagName | startswith("'"$PACKAGE"'@")) | .tagName | sub("'"$PACKAGE"'@";"") | select(length > 0) | split(".") | map(tonumber)] | sort | last | if . == null or . == [] then [0,1,0] else . end | map(tostring) | join("."))' <<< "$RELEASES")"
   fi
 
-  echo "✅ Marking release $TAG_NAME as published"
-  if gh release view "$TAG_NAME" > /dev/null 2>&1; then
-    gh release edit "$TAG_NAME" --draft=false
+  echo "📦 Verifying $TAG_NAME"
+
+  #if pnpm run publish "$PACKAGE"; then
+    if [[ ! -f "docs/${TAG_NAME}.tgz" ]]; then
+      echo "❌ Archive $TAG_NAME not found"
+      exit 1
+    fi
+
+    echo "✅ Marking release $TAG_NAME as published"
+    if gh release view "$TAG_NAME" > /dev/null 2>&1; then
+      gh release edit "$TAG_NAME" --draft=false
+    else
+      echo "❌ Release $TAG_NAME does not exist, skipping edit."
+    fi
   else
-    echo "❌ Release $TAG_NAME does not exist, skipping edit."
-  fi
-else
-  echo "❌ Failed to publish $PACKAGE"
-  exit 1
-fi
+    echo "❌ Failed to publish $TAG_NAME"
+    exit 1
+  #fi
+done
+
